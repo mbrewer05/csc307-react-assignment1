b43791bf435dd1a075cb133ab87944844f46db80
Maxwell Brewer <44121859+mbrewer05@users.noreply.github.com>
2021-04-04 21:05:22 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import MyApp from './MyApp'
+import './index.css'
+
+
+ReactDOM.render(<MyApp />, document.getElementById('root'))
