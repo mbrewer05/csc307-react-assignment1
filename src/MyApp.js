f515f0756772d6ee368a95a24ea9a80200a26ee5
Maxwell Brewer <44121859+mbrewer05@users.noreply.github.com>
2021-04-08 22:22:01 -0700

    added step 1

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Table from './Table';
 import Form from './Form';
 
@@ -13,8 +14,41 @@ function MyApp() {
     };
     
     function updateList(person) {
-        setCharacters([...characters, person]);
+        makePostCall(person).then(result => {
+            if (result)
+                setCharacters([...characters, person]);
+        });
     };
+    
+    async function fetchAll(){
+        try {
+            const response = await axios.get('http://localhost:5000/users');
+            return response.data.users_list; 
+        }
+        catch (error){
+            //We're not handling errors. Just logging into the console.
+            console.log(error); 
+            return false;         
+        }
+    };
+    
+    async function makePostCall(person){
+        try {
+            const response = await axios.post('http://localhost:5000/users', person);
+            return response;
+        }
+        catch (error) {
+            console.log(error);
+            return false;
+        }
+    };
+    
+    useEffect(() => {
+        fetchAll().then(result => {
+            if (result)
+                setCharacters(result);
+        });
+    }, [] );
   
     return ( 
       <div className="container"> 
